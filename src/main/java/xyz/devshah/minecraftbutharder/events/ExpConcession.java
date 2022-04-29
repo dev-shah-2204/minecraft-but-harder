cfe8ebde5b9d541cf6450293a8f6444208d08934
dev-shah-2204 <dgm82382@gmail.com>
2022-04-29 23:20:09 +0530

    Players don't lose xp on using anvil

--- src/main/java/xyz/devshah/minecraftbutharder/events/ExpConcession.java
+++ src/main/java/xyz/devshah/minecraftbutharder/events/ExpConcession.java
@@ -1,15 +1,61 @@
 package xyz.devshah.minecraftbutharder.events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.AnvilInventory;
+import xyz.devshah.minecraftbutharder.MinecraftButHarder;
+
+import java.util.HashMap;
 
 public class ExpConcession implements Listener {
+    MinecraftButHarder plugin;
+    public ExpConcession(MinecraftButHarder plugin) { this.plugin = plugin; }
+
+    private final HashMap<String, Integer> levels = new HashMap<>();
 
     @EventHandler
     public void onPlayerEnchantItem(EnchantItemEvent event) {
         // Don't ask me how, but this logic works flawlessly.
         event.getEnchanter().setLevel(event.getEnchanter().getLevel() + event.whichButton() + 1);
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (event.getInventory() instanceof AnvilInventory) {
+            Player player = (Player) event.getPlayer();
+
+            if (levels.containsKey(player.getDisplayName())) {
+                player.setLevel(levels.get(player.getDisplayName()));
+                levels.remove(player.getDisplayName());
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getInventory() instanceof AnvilInventory) {
+            Player player = (Player) event.getWhoClicked();
+
+            if (!(levels.containsKey(player.getDisplayName()))) {
+                levels.put(player.getDisplayName(), player.getLevel());
+            }
+
+            if (event.getRawSlot() != 2) player.setLevel(201);
+
+            if (event.getRawSlot() == 2) {
+                int level = levels.get(player.getDisplayName());
+                levels.remove(player);
+
+                Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {player.setLevel(level);}, 1);
+
+            }
+        }
     }
 
 }
