d7075ec815565e672ee227147ed1553d6e4de949
dev-shah-2204 <dgm82382@gmail.com>
2022-04-29 21:35:00 +0530

    Fix bug where players lost levels

--- src/main/java/xyz/devshah/minecraftbutharder/events/Graves.java
+++ src/main/java/xyz/devshah/minecraftbutharder/events/Graves.java
@@ -1,5 +1,6 @@
 package xyz.devshah.minecraftbutharder.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
@@ -9,50 +10,57 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
+import xyz.devshah.minecraftbutharder.MinecraftButHarder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Graves implements Listener {
+    MinecraftButHarder plugin;
+    public Graves(MinecraftButHarder plugin) { this.plugin = plugin; }
+
     private final HashMap<String, Integer> levels = new HashMap<>();
+
 
     @EventHandler
     public void onPlayerDeath(EntityDeathEvent event) {
         if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
 
-            double x = player.getLocation().getX();
-            double y = player.getLocation().getY();
-            double z = player.getLocation().getZ();
-
-            player.sendMessage("§aYour grave is at " + String.format("%.0f", x) + "/" + String.format("%.0f", y) + "/" + String.format("%.0f", z));
-            System.out.println("§a" + player.getDisplayName() + "'s grave is at " + String.format("%.0f", x) + "/" + String.format("%.0f", y) + "/" + String.format("%.0f", z));
-
-            Block deathBlock = player.getWorld().getBlockAt(player.getLocation());
-            if (deathBlock.getType() != Material.AIR) deathBlock.breakNaturally();
-            deathBlock.setType(Material.CHEST);
-            Chest left = (Chest) deathBlock.getState();
-
-            Block deathBlock2 = player.getWorld().getBlockAt(player.getLocation().add(1, 0, 0));
-            if (deathBlock2.getType() != Material.AIR) deathBlock2.breakNaturally();
-
-            deathBlock2.setType(Material.CHEST);
-            Chest right = (Chest) deathBlock2.getState();
-
-            List<ItemStack> items = new ArrayList<>(event.getDrops());
-            event.getDrops().clear();
-
-            int i = 0;
-
-            for (ItemStack item : items) {
-                if (i <= 26) left.getInventory().setItem(i, item);
-                else right.getInventory().setItem(i-27, item);
-                if (i != items.size()) i++;
-            }
-
             event.setDroppedExp(0);
             levels.put(player.getDisplayName(), player.getLevel());
+
+            if (event.getDrops().size() > 0) {
+                double x = player.getLocation().getX();
+                double y = player.getLocation().getY();
+                double z = player.getLocation().getZ();
+
+                player.sendMessage("§aYour grave is at " + String.format("%.0f", x) + "/" + String.format("%.0f", y) + "/" + String.format("%.0f", z));
+                System.out.println("§a" + player.getDisplayName() + "'s grave is at " + String.format("%.0f", x) + "/" + String.format("%.0f", y) + "/" + String.format("%.0f", z));
+
+                Block deathBlock = player.getWorld().getBlockAt(player.getLocation());
+                if (deathBlock.getType() != Material.AIR) deathBlock.breakNaturally();
+                deathBlock.setType(Material.CHEST);
+                Chest left = (Chest) deathBlock.getState();
+
+                Block deathBlock2 = player.getWorld().getBlockAt(player.getLocation().add(1, 0, 0));
+                if (deathBlock2.getType() != Material.AIR) deathBlock2.breakNaturally();
+
+                deathBlock2.setType(Material.CHEST);
+                Chest right = (Chest) deathBlock2.getState();
+
+                List<ItemStack> items = new ArrayList<>(event.getDrops());
+                event.getDrops().clear();
+
+                int i = 0;
+
+                for (ItemStack item : items) {
+                    if (i <= 26) left.getInventory().setItem(i, item);
+                    else right.getInventory().setItem(i - 27, item);
+                    if (i != items.size()) i++;
+                }
+            }
 
         }
     }
@@ -60,8 +68,11 @@ public class Graves implements Listener {
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
+
+        player.setLevel(2);
         if (levels.containsKey(player.getDisplayName())) {
-            player.setLevel(levels.get(player.getDisplayName()));
+            int level = levels.get(player.getDisplayName());
+            Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {player.setLevel(level);}, 2);
             levels.remove(player.getDisplayName());
         }
     }
