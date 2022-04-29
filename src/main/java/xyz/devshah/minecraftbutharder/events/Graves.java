cfca4b25df2f32e95b8b90235ebca0a9662a7e5a
dev-shah-2204 <dgm82382@gmail.com>
2022-04-29 12:39:05 +0530

    Reduce lines?

--- src/main/java/xyz/devshah/minecraftbutharder/events/Graves.java
+++ src/main/java/xyz/devshah/minecraftbutharder/events/Graves.java
@@ -3,7 +3,6 @@ package xyz.devshah.minecraftbutharder.events;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +13,6 @@ import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
 
 public class Graves implements Listener {
     private final HashMap<String, Integer> levels = new HashMap<>();
@@ -32,35 +30,24 @@ public class Graves implements Listener {
             System.out.println("§a" + player.getDisplayName() + "'s grave is at " + String.format("%.0f", x) + "/" + String.format("%.0f", y) + "/" + String.format("%.0f", z));
 
             Block deathBlock = player.getWorld().getBlockAt(player.getLocation());
-            if (deathBlock.getType() != Material.AIR) {
-                deathBlock.breakNaturally();
-            }
+            if (deathBlock.getType() != Material.AIR) deathBlock.breakNaturally();
             deathBlock.setType(Material.CHEST);
             Chest left = (Chest) deathBlock.getState();
-            left.setCustomName(player.getDisplayName() + "'s grave 1");
 
             Block deathBlock2 = player.getWorld().getBlockAt(player.getLocation().add(1, 0, 0));
-            if (deathBlock2.getType() != Material.AIR) {
-                deathBlock2.breakNaturally();
-            }
+            if (deathBlock2.getType() != Material.AIR) deathBlock2.breakNaturally();
+
             deathBlock2.setType(Material.CHEST);
             Chest right = (Chest) deathBlock2.getState();
-            right.setCustomName(player.getDisplayName() + "'s grave 2");
 
-
-            List<ItemStack> items = new ArrayList<>();
-            for (ItemStack thing : event.getDrops()) items.add(thing);
+            List<ItemStack> items = new ArrayList<>(event.getDrops());
             event.getDrops().clear();
 
             int i = 0;
 
             for (ItemStack item : items) {
-                if (i <= 26) {
-                    left.getInventory().setItem(i, item);
-                }
-                else if (i > 26) {
-                    right.getInventory().setItem(i-27, item);
-                }
+                if (i <= 26) left.getInventory().setItem(i, item);
+                else right.getInventory().setItem(i-27, item);
                 if (i != items.size()) i++;
             }
 
